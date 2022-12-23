f161b25259ccd4c64e7ba9befff7b515b0d89fe0
Robert Onstott <roonstott@gmail.com>
2022-12-23 12:03:13 -0800

    Add setup code for csproj and context

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public int StylistId { get; set; }
+    public Stylist Stylist { get; set; }
+  }
+}
